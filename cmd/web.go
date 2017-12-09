b623bb6b8cb94a768e0d47227a60940cb06ed8ab
Max Obermeier <maxo2000@outlook.de>
2017-12-09 16:24:23 +0100

    set up basic interfaces and structures

---
+++ cmd/web.go
@@ -0,0 +1,3 @@
+package cmd
+
+//"gopkg.in/urfave/cli.v2"
---
+++ storage/storage.go
@@ -0,0 +1,30 @@
+package storage
+
+import "io"
+
+type Revision interface {
+}
+
+type Storage interface {
+	Root(revision Revision) Topic
+	Sync()
+	GetRevisions()
+}
+
+// Topic depicts an article and its subtopics
+type Topic interface {
+	// ID returns an un-localized name for the article
+	ID() string
+
+	// Locales returns all available locales
+	Locales() []string
+
+	// Name returns the localized Name
+	Name(locale string) string // non-unique name
+
+	// Data returns a rewinded io.Reader delivering the markdown code
+	Data(locale string) io.Reader // link
+
+	// Subtopics returns the subtopics of the topic
+	Subtopics() []Topic // subtopics
+}
