60ec1c8c254eba2809f7870aee9b79f70cffdcc2
Max Obermeier <themomax@icloud.com>
2017-12-12 20:14:37 +0100

    improved handling of third-party modules

--- generate.go
+++ generate.go
@@ -4,10 +4,22 @@ import (
 	_ "github.com/tdewolff/minify"
 )
 
-// go get -u github.com/tdewolff/minify/cmd/minify
+//go:generate echo ------------------------------------------------------------------------
+//go:generate echo
+//go:generate echo This file (generate.go) includes all necessary go generate commands of
+//go:generate echo this project. While some of them only have to be run when initializing
+//go:generate echo the project on your machine and others more frequently. Just disable
+//go:generate echo the ones you don't need by removing the "go:generate" part.
+//go:generate echo
+//go:generate echo ------------------------------------------------------------------------
 
-// wget -O static/extensions/katex/katex.min.js https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0-alpha2/katex.min.js
-// wget -O static/extensions/katex/katex.min.css https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0-alpha2/katex.min.css
-// wget -O static/extensions/katex-autorender/katex-autorender.min.js https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0-alpha2/contrib/auto-render.min.js
+// ------ project initialization ------------------------------------------
+//
+//go:generate go get .
+//go:generate wget -O static/extensions/katex/katex.min.js https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0-alpha2/katex.min.js
+//go:generate wget -O static/extensions/katex/katex.min.css https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0-alpha2/katex.min.css
+//go:generate wget -O static/extensions/katex-autorender/katex-autorender.min.js https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0-alpha2/contrib/auto-render.min.js
 
+// ------ others ----------------------------------------------------------
+//
 //go:generate minify -r --match .+\.js static/js/doc static/extensions/katex static/js static/extensions/katex-autorender -o static/js/doc.min.js
